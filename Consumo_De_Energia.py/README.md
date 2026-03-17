6abfb6178ea6f1abe8594fc1c3f560df857fa745
VictorEmanuelCaetano <139991133+VictorEmanuelCaetano@users.noreply.github.com>
2026-03-17 18:43:08 -0300

    feat: versão 1

---
+++ Consumo_De_Energia.py/README.md
@@ -0,0 +1,7 @@
+Calculador de consumo
+
+recebe os valores por meio de input, verifica se há erros de ortografia e depois verica se os valores estão corretos. Caso algum valor esteja errado retorna o loop do começo.
+
+funções:
+calcula o consumo de um aparelho em kWh/mês
+calcula o preco estimado para ter esse aparelho ligado por x tempo.
--- Consumo_De_Energia.py/app.py
+++ Consumo_De_Energia.py/app.py
@@ -1,13 +1,38 @@
 def pegar_informacoes():
-    # Cuida de erros e pega o valor inicial das variáveis.  
-    try:
+    # Cuida de erros e pega o valor inicial das variáveis.
+    while True:
         nome = input("Qual é o nome do aparelho? ")
-        potencia = float(input("Qual é a potência do aparelho em watts? "))
-        tempo_de_uso = float(input("Qual é o tempo de uso? "))
-    except ValueError:
-        print("Erro: Você deve inserir um valor numérico!")
-        pegar_informacoes()
-        return
-    
-pegar_informacoes()
-    
+        try:
+            potencia = float(input("Qual é a potência do aparelho em watts? "))
+            horas_de_uso = float(input("Qual é o tempo de uso por dia em horas? "))
+            preco_do_kw = float(input("Qual é o preço do kW da sua cidade? Obs: Se você não sabe apenas digite 0 para estimar uma média de preço: "))
+        except ValueError:
+            print("Erro: Você deve inserir um valor numérico!")
+            continue
+        if preco_do_kw == 0:
+            preco_do_kw = 0.7
+        
+        if potencia <= 0:
+            print("Erro: Potencia não pode ser menor que 1")
+            continue
+        
+        if horas_de_uso <= 0 or horas_de_uso > 24:
+            print("Erro: Tempo não pode ser menor que 1 ou maior que 24")
+            continue
+        
+        if preco_do_kw <= 0:
+            print("Erro: Preço não pode ser menor que 1")
+            continue
+        
+        return nome, potencia, horas_de_uso, preco_do_kw
+
+nome, potencia, horas_de_uso, preco_do_kw = pegar_informacoes()
+#calcula o consumo e valor
+def calcular(potencia, horas_de_uso, preco_do_kw):
+    consumo = (potencia * horas_de_uso * 30) / 1000
+    valor = consumo * preco_do_kw
+    return consumo, valor
+
+consumo, valor = calcular(potencia, horas_de_uso, preco_do_kw)
+#escreve o resultado na tela
+print(f"Aparelho: {nome}, Consumo: {consumo} kWh/mês, Preço estimado: R${valor:.2f} ")
